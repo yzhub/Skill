5e5aa1e7440e122da1a4a1655123b77378563901
U-EASYTECH-MAIN\yangyaozhong <yangyaozhong@liumeihua.easytech-main.com>
2017-03-27 09:19:47 +0800

    语言转换

---
+++ lang/en.php
@@ -0,0 +1,13 @@
+ <?php
+ return array(
+  'How are you' =>'你好吗',
+  'Nice to meet you'=>'很高兴见到你',
+  'Nice to see you again' =>'很高兴再见到你' , 
+  'How have you been?'=>'你过得怎么样 ' ,
+  'Long time no see'=>'好久不见',
+  'How is it going'=>'近况如何'  ,
+  'How’s everything with you'=>'你的一切如何' ,
+  'Hi! Are you having fun'=>'嗨，你过得愉快吗' ,
+  'How have you been'=>'你近来如何',
+  'So far so good'=>'到目前还好'  
+  );
